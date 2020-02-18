11e34413d4a1510bfb034b673ddfdac8d11e201a
Olwal002 <olwalevance0@gmail.com>
2020-02-18 12:27:00 +0300

    building a form sample

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FormContainer from "./FormContainer";
+
+function app() {
+  return (
+    <div>
+      <FormContainer />
+    </div>
+  );
+}
+export default app;
